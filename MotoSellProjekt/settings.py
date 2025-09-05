863a54d847a98976978b095921caa89f66d50e10
Przemysław Matusiewicz <przemyslawmatusiewicz988@gmail.com>
2025-09-05 21:57:47 +0200

    Czytanie SECRET_KEY z .env

--- MotoSellProjekt/settings.py
+++ MotoSellProjekt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hg%&46dkjpk0x+1ray9hosve0vjp&_b6tnlgc%9p*49$j6hr%k'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
