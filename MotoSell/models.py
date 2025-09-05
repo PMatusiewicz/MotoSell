e6302a20ccb38f251c075eb10ea4dc7eacf2482d
Przemysław Matusiewicz <przemyslawmatusiewicz988@gmail.com>
2025-09-05 02:01:50 +0200

    Dodanie modelu pojazdu

--- MotoSell/models.py
+++ MotoSell/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Pojazd(models.Model):
+    KATEGORIE = {
+        "motocykl": "Motocykl",
+        "osobowy": "Osobowy",
+        "ciezarowy": "Ciężarowy"
+    }
+    PALIWO = {
+        "benzyna": "Benzyna",
+        "diesel": "Diesel",
+        "lpg": "LPG"
+    }
+
+    tytul = models.CharField(max_length=100)
+    opis = models.TextField()
+    kategoria = models.CharField(max_length=10, choices=KATEGORIE.items())
+    marka = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    rok_produkcji = models.PositiveIntegerField()
+    przebieg = models.PositiveIntegerField()
+    pojemnosc_skokowa = models.PositiveIntegerField()
+    rodzaj_paliwa = models.CharField(max_length=10, choices=PALIWO.items())
+    uzytkownik = models.ForeignKey(User, on_delete=models.CASCADE)
+    #zdjecie =
+    #TODO
+    data_dodania = models.DateField()
+    data_publikacji = models.DateField(null=True)
