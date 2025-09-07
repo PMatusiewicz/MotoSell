a472d68a5b64986f4ffa309f9b64a22e58cd1de9
Przemysław Matusiewicz <przemyslawmatusiewicz988@gmail.com>
2025-09-08 01:08:01 +0200

    Dodano możliwość rejestracji

--- MotoSell/views.py
+++ MotoSell/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, "MotoSell/index.html")
+    uzytkownik = request.user
+    return render(request, "MotoSell/index.html", {
+        "uzytkownik": uzytkownik
+    })
+
+def rejestracja(request):
+    if request.method == "POST":
+        formularz_rejestracji = UserCreationForm(request.POST)
+        if formularz_rejestracji.is_valid():
+            nowy_uzytkownik = formularz_rejestracji.save()
+            login(request, nowy_uzytkownik)
+            return redirect('index')
+    else:
+        formularz_rejestracji = UserCreationForm()
+
+    return render(request, "MotoSell/rejestracja.html", {
+        "formularz_rejestracji": formularz_rejestracji
+    })
