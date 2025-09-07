961d67759d27fd02cd69e3747a6752b3b20f8a0d
Przemysław Matusiewicz <przemyslawmatusiewicz988@gmail.com>
2025-09-07 21:28:27 +0200

    Uzupełnienie modelu Pojazd i skonfigurowanie zdjęć do modelu

--- MotoSellProjekt/urls.py
+++ MotoSellProjekt/urls.py
@@ -21,5 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
