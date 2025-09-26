ad5e0c60e7d3fcf12af3416cae21fdc4caf56df5
Przemysław Matusiewicz <przemyslawmatusiewicz988@gmail.com>
2025-09-26 03:41:28 +0200

    Dodano aplikację angular i komponent odpowiadający za widok publicznych ofert

--- MotoSellProjekt/MotoSellProjekt/settings.py
+++ MotoSellProjekt/MotoSellProjekt/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MotoSell',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
